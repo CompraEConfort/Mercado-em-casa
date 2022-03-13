6eaacd7d30db7b0792cce49509e0070e0133a40b
Alvaro-Eduardo482 <alvaro.santana1@hotmail.com>
2022-03-13 14:22:14 -0300

    Adição de comentários de guia 13/03/22

--- service/axiosService.js
+++ service/axiosService.js
@@ -4,7 +4,7 @@ function cadastroService (requestBody) {
     axios.post('http://localhost:3000/usuarios/cadastro', requestBody)
     .then(res => {
         console.log(res)
-        window.location ("http://localhost/Mercado-em-casa/Telalogin/Perfil.html")
+       // window.location ("http://localhost/Mercado-em-casa/Telalogin/Perfil.html")
     })
     .catch(err => {
         console.log(err)
@@ -16,18 +16,47 @@ function loginService (requestBody) {
     axios.post('http://localhost:3000/usuarios/login', requestBody)
     .then(res => {
         console.log(res)
+        alert("Logado com sucesso !")
+        const token = res.token
+        // 1, Guardar o token no Cache/Local Storage
+        // 2, Fazer o redirect para tela
         
+        // 2 PARTE
+        // Vai pegar o token do local storage
+        // Vai montar a requisição com o token
+        // Fazer a chamada ao endpoint GET usuarios/perfil/
+        // Pegar o body/corpo da requisição que irá conter as informações do usuário
+        // Preencher os campos da tela com as informações do usuário, (Foto, nome e etc)
     })
     .catch(err => {
         console.log(err)
-        //alert("Falha no login, Usuario ou senha invalidos") 
+        alert("Falha no login, Usuario ou senha invalidos") 
     })
 }
+
+function profileService() {
+    // Vai pegar o token do local storage
+    // Vai montar a requisição com o token
+    // Fazer a chamada ao endpoint GET usuarios/perfil/
+    
+    await axios.get('http://localhost:3000/usuarios/perfil', {
+        headers: {
+          'authorization': `Bearer ${token}`
+        }
+    })
+    .then(res => {
+        return res.data
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
 function cadastroMercadoService (requestBody) {
     axios.post('http://localhost:3000/supermercados/cadastro', requestBody)
     .then(res => {
         console.log(res)
-        window.location.href = "http://localhost/Mercado-em-casa/Resto-do-site/produtos.html?"
+       // window.location.href = "http://localhost/Mercado-em-casa/Resto-do-site/produtos.html?"
     })
     .catch(err => {
         console.log(err)
