a3425e7e16a84fd98bfe3568aad09f41279cf280
Alvaro-Eduardo482 <alvaro.santana1@hotmail.com>
2022-04-23 09:55:27 -0300

    Atualização da tela de produtos

--- Resto-do-site/js/produtos.js
+++ Resto-do-site/js/produtos.js
@@ -3,7 +3,7 @@
     var nomeCorredor = localStorage.getItem("nome-corredor");
     var produtos = getprodutosNome(nomeCorredor);
     //var produtos = getProductsByCategoryAndSupermarketId();
-    var divprodutos = document.getElementById('produtos');
+    var divprodutos = document.getElementById('products-template');
     var produtosDinamico = '';
     document.getElementById('Market-title').innerHTML = nomeCorredor;
     // document.getElementById('Market-Name').innerHTML = nomeCorredor;
@@ -11,20 +11,37 @@
     produtos.produtos.forEach(element => {
         console.log(element)
 
-        produtosDinamico +=  `<div class="col-lg-4 col-md-6 col-sm-6">`
-        produtosDinamico +=      `<div class="product__item">`
-        produtosDinamico +=         `<div class="product__item__pic set-bg">`
-        produtosDinamico +=         `<img src="${element.imageLink}" alt="">`
-        produtosDinamico +=            `<ul class="product__item__pic__hover">`
-        produtosDinamico +=               `<li onclick="salvarProdutoNoCarrinho(${element.idProduto},${element.imageLink},${element.name},${element.value})"><i class="fa fa-shopping-cart"></i></li>`
-        produtosDinamico +=            `</ul>`
-        produtosDinamico +=         `</div>`
-        produtosDinamico +=         `<div class="product__item__text">`
-        produtosDinamico +=            `<h6><a href="#">${element.name}</a></h6>`
-        produtosDinamico +=            `<h5>R$ ${element.value}</h5>`
+        // produtosDinamico +=  `<div class="col-lg-4 col-md-6 col-sm-6">`
+        // produtosDinamico +=      `<div class="product__item">`
+        // produtosDinamico +=         `<div class="product__item__pic set-bg">`
+        // produtosDinamico +=         `<img src="${element.imageLink}" alt="">`
+        // produtosDinamico +=            `<ul class="product__item__pic__hover">`
+        // produtosDinamico +=               `<li onclick="salvarProdutoNoCarrinho(${element.idProduto},${element.imageLink},${element.name},${element.value})"><i class="fa fa-shopping-cart"></i></li>`
+        // produtosDinamico +=            `</ul>`
+        // produtosDinamico +=         `</div>`
+        // produtosDinamico +=         `<div class="product__item__text">`
+        // produtosDinamico +=            `<h6><a href="#">${element.name}</a></h6>`
+        // produtosDinamico +=            `<h5>R$ ${element.value}</h5>`
+        // produtosDinamico +=         `</div>`
+        // produtosDinamico +=      `</div>`
+        // produtosDinamico +=  `</div>`
+
+        produtosDinamico +=  `<div class="column" style="width: 30.6666666667%;">`
+        produtosDinamico +=     `<div class="simpleCart_shelfItem">`
+        produtosDinamico +=         `<img src="${element.imageLink}"/>`
+        produtosDinamico +=         `<div class="row">`
+        produtosDinamico +=            `<h5 class="item_name"></h5>`
+        produtosDinamico +=             `<div class="simpleStore_getDetail_container">`
+        produtosDinamico +=               `<span class="item_price"></span>`
+        produtosDinamico +=            `</div>`
+        produtosDinamico +=               `<div class="simpleStore_getDetail_container">`
+        produtosDinamico +=                   `<a class="button u-pull-right simpleStore_getDetail">Adicionar</a>`
+        produtosDinamico +=                 `</div>`
+        produtosDinamico +=            `</div>`
         produtosDinamico +=         `</div>`
         produtosDinamico +=      `</div>`
-        produtosDinamico +=  `</div>`
+    
+            
 
     });
     console.log("Após a chamada da function")
